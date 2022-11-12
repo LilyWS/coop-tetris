1d4822fa162058ce74fea091ae83945c001187ef
LilyWS <stranger1212ss23@gmail.com>
2022-11-12 00:46:18 -0500

    seeded database

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,3 +2,14 @@ const db = require('../config/connection');
 const { Game } = require('../models');
 
 const gameData = require('./gameData.json');
+
+db.once('open', async () => {
+    // clean database
+    await Game.deleteMany({});
+  
+    // bulk create each model
+    const games = await Game.insertMany(gameData);
+  
+    console.log('all done!');
+    process.exit(0);
+  });
